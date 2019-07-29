d7b4f33f47c03c59e4e5f77ba2bd7fd9c5ff58fd
Daniel Prilik <danielprilik@gmail.com>
2019-07-29 15:59:42 -0400

    chapter 5: multiple objects _and_ normals :O
    Gonna do a quick pass to clean the code up and make it a bit rusty

--- src/render.rs
+++ src/render.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use crate::ray::Ray;
 use crate::vec3::Vec3;
 
@@ -12,22 +14,97 @@ impl AsColor for Vec3 {
     }
 }
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool {
-    let oc = r.origin() - *center;
-    let a = Vec3::dot(&r.direction(), &r.direction());
-    let b = 2.0 * Vec3::dot(&oc, &r.direction());
-    let c = Vec3::dot(&oc, &oc) - radius * radius;
-    let discriminant = b.powf(2.) - 4. * a * c;
-
-    discriminant > 0.0
+#[derive(Debug, Copy, Clone)]
+pub struct HitRecord {
+    /// Position along ray
+    pub t: f32,
+    /// Hit point
+    pub p: Vec3,
+    /// Hit Normal
+    pub normal: Vec3,
 }
 
-fn color(r: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0);
+trait Hitable: std::fmt::Debug {
+    fn hit(&self, r: &Ray, t_range: Range<f32>, rec: &mut HitRecord) -> bool;
+}
+
+#[derive(Debug)]
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f32,
+}
+
+impl Sphere {
+    fn new(center: Vec3, radius: f32) -> Sphere {
+        Sphere { center, radius }
+    }
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, r: &Ray, t_range: Range<f32>, rec: &mut HitRecord) -> bool {
+        let oc = r.origin() - self.center;
+        let a = Vec3::dot(&r.direction(), &r.direction());
+        let b = 2.0 * Vec3::dot(&oc, &r.direction());
+        let c = Vec3::dot(&oc, &oc) - self.radius.powf(2.);
+        let discriminant = b.powf(2.) - 4. * a * c;
+
+        if discriminant > 0.0 {
+            macro_rules! check_root {
+                ($sign:tt) => {
+                    let root = (-b $sign discriminant.sqrt()) / (2.0 * a);
+                    if t_range.contains(&root) {
+                        rec.t = root;
+                        rec.p = r.point_at_param(rec.t);
+                        rec.normal = (rec.p - self.center) / self.radius;
+                        return true;
+                    }
+                };
+            }
+            check_root!(-);
+            check_root!(+);
+        }
+
+        false
+    }
+}
+
+impl Hitable for Vec<Box<dyn Hitable>> {
+    fn hit(&self, r: &Ray, t_range: Range<f32>, rec: &mut HitRecord) -> bool {
+        // TODO: this be ugly
+        let mut temp_rec = HitRecord {
+            t: 0.0,
+            p: Vec3::new(0., 0., 0.),
+            normal: Vec3::new(1., 0., 0.),
+        };
+        let mut hit_anything = false;
+
+        let mut closest_so_far = t_range.end;
+        for hitable in self {
+            if hitable.hit(r, t_range.start..closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+        hit_anything
+    }
+}
+
+fn color(r: &Ray, world: &Vec<Box<dyn Hitable>>) -> Vec3 {
+    // TODO: this be ugly
+    let mut rec = HitRecord {
+        t: 0.0,
+        p: Vec3::new(0., 0., 0.),
+        normal: Vec3::new(1., 0., 0.),
+    };
+
+    if world.hit(r, 0.0..std::f32::MAX, &mut rec) {
+        let n = rec.normal;
+        return 0.5 * Vec3::new(n.x + 1., n.y + 1., n.z + 1.);
     }
 
-    let unit_direction = Vec3::normalize(&r.direction());
+    // Background gradient
+    let unit_direction = r.direction().normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
@@ -37,6 +114,11 @@ pub fn trace_some_rays(buffer: &mut Vec<u32>, width: usize, height: usize) {
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+
+    let world: Vec<Box<dyn Hitable>> = vec![
+        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)),
+        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)),
+    ];
 
     for (y, row) in buffer.chunks_exact_mut(width).enumerate() {
         for (x, px) in row.iter_mut().enumerate() {
@@ -48,7 +130,7 @@ pub fn trace_some_rays(buffer: &mut Vec<u32>, width: usize, height: usize) {
 
             let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
 
-            *px = color(&r).as_color();
+            *px = color(&r, &world).as_color();
         }
     }
 }
